5cbe0b504edf9c1eb772e1772fd58d58b7429326
Andrey Shahov <orionshah@yandex.ru>
2015-11-13 09:58:47 -0500

    Логика добавления новой базы в базу(lolwat)

--- client/client.js
+++ client/client.js
@@ -1,7 +1,7 @@
 if (Meteor.isClient) {
     Bases = new Meteor.Collection('bases');
     Meteor.subscribe("bases");
-    
+
     Router.configure({
         autoRender: false,
     });
@@ -19,6 +19,38 @@ if (Meteor.isClient) {
         //     {about: "vdsgerty5y gr", name: "base5", id: 5},
         // ]
     });
+    Template.filters.helpers({
+        filters: [
+            {id: 1, text:"fil1", active: 1},
+            {id: 2, text:"fil2", active: 0},
+            {id: 3, text:"fil3", active: 0},
+            {id: 4, text:"fil4", active: 0},
+            {id: 5, text:"fil5", active: 0},
+            {id: 6, text:"fil6", active: 0},
+            {id: 7, text:"fil7", active: 0},
+            {id: 8, text:"fil8", active: 0},
+        ]
+    });
+    Template.filters.events({
+        'click .filter': function () {
+            console.log(this);
+            this.active = !this.active;
+            console.log(this);
+
+        }
+    });
+    Template.new.events({
+        'submit #new': function (e) {
+            e.preventDefault();
+            var inputs = $("input[name^='base']");
+            console.log(inputs);
+            $.each(inputs, function(index, val) {
+                full_name = val.name;
+                console.log(full_name.substring(full_name.indexOf('.')+1, full_name.length));
+
+            });
+        }
+    });
     Router.route('/base/:_id', function() {
         var item = Bases.findOne({id: this.params._id});
         this.render('base', {data: item});
@@ -33,5 +65,8 @@ if (Meteor.isClient) {
     Router.route('/about', function() {
         this.render('about');
     });
+    Router.route('/new', function() {
+        this.render('new');
+    })
 }
 
